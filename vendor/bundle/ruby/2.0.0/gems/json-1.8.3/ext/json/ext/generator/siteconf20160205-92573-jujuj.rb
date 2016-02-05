af5263eeeddba71d3ae626d73d1ef49759e75c45
Patrick Lau <patricklau@wpa182012.wireless.mcgill.ca>
2016-02-05 17:12:45 -0500

    Test4

---
+++ vendor/bundle/ruby/2.0.0/gems/json-1.8.3/ext/json/ext/generator/siteconf20160205-92573-jujuj.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160205-92573-c428uz"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
---
+++ vendor/bundle/ruby/2.0.0/gems/nokogiri-1.6.7.2/ext/nokogiri/siteconf20160205-92573-7p33qp.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20160205-92573-8ijyc7"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
